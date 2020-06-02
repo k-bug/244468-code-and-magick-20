81b578bafef12523dafc5240942a6f62318c201a
Kirill Bugaev <bugaev.kirill@gmail.com>
2020-06-02 17:08:55 +0300

    Adds red color

--- js/stat.js
+++ js/stat.js
@@ -3,9 +3,7 @@
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
-var CLOUD_Y = 50;
 var GAP = 20;
-
 var MAX_BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
@@ -27,25 +25,20 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-var getPlayersColor = function (names) {
-  // var color = 'hsla(244, 100%, 50%, Math.random())';
-  var color = 'hsla(244, 100%, 50%, 1)';
-
-  for (var i = 0; i < names.length; i++) {
-    if (names[i] === 'Вы') {
-      color = 'rgba(255, 0, 0, 1)';
-      console.log('if ' + color);
-    }
-    else {
-      color = 'hsla(244, 100%, 50%, 1)';
-      console.log('else ' + color);
-    }
-  }
-
-  console.log('if result ' + color);
-
-  return color;
-}
+// var getPlayersColor = function (names) {
+//   // var color = 'hsla(244, 100%, 50%, Math.random())';
+//   var color = 'hsla(244, 100%, 50%, 1)';
+//
+//   for (var i = 0; i < names.length; i++) {
+//     if (names[i] === 'Вы') {
+//       color = 'rgba(255, 0, 0, 1)';
+//     } else {
+//       color = 'hsla(244, 100%, 50%, 1)';
+//     }
+//   }
+//
+//   return color;
+// };
 
 var renderResults = function (ctx, players, times) {
   ctx.fillStyle = '#000';
@@ -56,14 +49,16 @@ var renderResults = function (ctx, players, times) {
   ctx.fillText('Список результатов:', CLOUD_X + GAP, 30 + GAP);
 
   var maxTime = getMaxElement(times);
-  var playerColor = getPlayersColor(players);
 
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = '#000';
     ctx.fillText(players[i], CLOUD_X + GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP);
-    ctx.fillStyle = playerColor;
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'hsla(244, 100%, 50%, 1)';
+    }
     ctx.fillRect(CLOUD_X + GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP * 1.5, BAR_WIDTH, -(MAX_BAR_HEIGHT * times[i]) / maxTime);
-    console.log(playerColor);
   }
 };
 
